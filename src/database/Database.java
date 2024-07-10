5fbcde047fe31ccb7ea9a02eecc9f99dba1536d1
Didi Prasetyo <didiprasetyo360@gmail.com>
2024-07-10 17:52:00 +0700

    update admin

--- src/database/Database.java
+++ src/database/Database.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package database;
 
 import com.mysql.cj.jdbc.MysqlDataSource;
@@ -25,31 +24,3 @@ public class Database {
         }
     }
 }
-=======
-package database;
-
-import com.mysql.cj.jdbc.MysqlDataSource;
-import java.sql.SQLException;
-
-public class Database {
-    private static java.sql.Connection connection;
-
-    public static java.sql.Connection getConnection() throws SQLException {
-        if (connection == null || connection.isClosed()) {
-            MysqlDataSource dataSource = new MysqlDataSource();
-            dataSource.setURL("jdbc:mysql://localhost:3306/projectakhir?zeroDateTimeBehavior=CONVERT_TO_NULL");
-            dataSource.setUser("root");
-            dataSource.setPassword("password");
-
-            connection = dataSource.getConnection();
-        }
-        return connection;
-    }
-
-    public static void closeConnection() throws SQLException {
-        if (connection != null && !connection.isClosed()) {
-            connection.close();
-        }
-    }
-}
->>>>>>> main
--- src/models/Admin.java
+++ src/models/Admin.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD:src/models/Admin.java
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
@@ -52,54 +51,3 @@ public class Admin {
         this.email = email;
     }
 }
-=======
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package models;
-
-/**
- *
- * @author adria
- */
-public class User {
-    private int id;
-    private String name;
-    private String email;
-
-    // Constructors
-    public User() {}
-
-    public User(int id, String name, String email) {
-        this.id = id;
-        this.name = name;
-        this.email = email;
-    }
-
-    // Getters and Setters
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-}
->>>>>>> main:src/models/User.java
